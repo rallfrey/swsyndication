f1a51558cb5017891d289efe896f1f290c45e212
Daniel Vydra <daniel.vydra@lonelyplanet.com.au>
2011-04-10 17:36:39 +1000

    fixed url shortener

--- src/com/swradioafrica/utils/UrlShortener.java
+++ src/com/swradioafrica/utils/UrlShortener.java
@@ -1,5 +1,12 @@
 package com.swradioafrica.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.google.appengine.repackaged.org.json.JSONObject;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
@@ -9,16 +16,31 @@ public class UrlShortener {
 	@Inject private PropertiesRepository propertiesRepository;
 	
 	public String shorten(String url) throws Exception {
-		String API_STRING = "http://api.j.mp/shorten?version=2.0.1&longUrl=%s&login=%s&apiKey=%s";
+		String API_STRING = "http://api.j.mp/v3/shorten?longUrl=%s&login=%s&apiKey=%s&format=json";
 		String USERNAME = propertiesRepository.loadProperties().JMPUsername;
 		String API_KEY = propertiesRepository.loadProperties().JMPKey;
 			
 		String api_url = String.format(API_STRING, url, USERNAME, API_KEY);
-		//HttpClient httpClient = new HttpClient();
-		//Response response = httpClient.get(api_url);
-		//JSONObject jsonObject = response.asJSONObject();
-		//String shortUrl = jsonObject.getJSONObject("results").getJSONObject(url).getString("shortUrl");
+		StringBuilder response = new StringBuilder();
 		
+        try {
+            URL jmpUrl = new URL(api_url);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(jmpUrl.openStream()));
+            String line;
+        	
+            while ((line = reader.readLine()) != null) {
+            	response.append(line);
+            }
+            reader.close();
+    		
+            JSONObject jsonObject = new JSONObject(response.toString());
+    		if (jsonObject.getString("status_txt").equals("OK")) {
+    			return jsonObject.getJSONObject("data").getString("url");
+    		}
+        } catch (Exception e) {
+            return "";
+        }
+
 		return "";
 	}
 }
